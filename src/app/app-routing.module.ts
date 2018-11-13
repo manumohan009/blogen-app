9208c892473bc28d3843069a57fdf34aef93af95
Manu <manumohan.cvav@gmail.com>
2018-11-13 23:29:41 +0530

    Moved app routes to a seperate module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { UserComponent } from './user/user.component';
+import { PostComponent } from './post/post.component';
+import { CategoryComponent } from './category/category.component';
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent, children: [] },
+  { path: 'category', component: CategoryComponent },
+  { path: 'post', component: PostComponent },
+  { path: 'user', component: UserComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
